02757df68e103896f472f0b6afd1092eea4c2ffa
abhii-shekk1311 <abhisheksaini.dev@gmail.com>
2025-05-25 22:49:20 +0530

    ADD: Added 404 page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='w-[100vw] bg-gray-300 text-gray-500 h-[100vh] flex flex-col items-center justify-center'>
+      <h2 className='text-2xl font-bold'>404 | Not Found!</h2>
+      <p>The page you’re looking for doesn’t exist.</p>
+    </div>
+  )
+}
+
+export default NotFound
